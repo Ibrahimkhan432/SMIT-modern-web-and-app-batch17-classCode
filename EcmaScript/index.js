379b09abbda3a6c098e65f69e3e358bdfcbf63cd
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-16 15:56:52 +0500

    javascript intro

--- EcmaScript/index.js
+++ EcmaScript/index.js
@@ -1,9 +1,34 @@
+// Intro 
+
+// Programming -- 
+
+// netscap (internet company) -- java-script
+
+// (oracle) -- sun microsystem -- java -- trademark
+
+// ECMA -- European Computer Manufacturer Association 
+
+// ECMAScript -- 4.1 stable --> 5 
+// book (A smart w.t.l js) -- ES5
+
+// c language (mother of the languages)
+
+// let , name , -- contain from c language 
+
+// translators:
+// 1)compiler  -- complete msg 
+// 2)interpreter -- direct execute (line by line)
+
+// JavaScript was just for browser (client side)
+// <script>
+
+// now for server-side (Node js)
+
+// Code
 // 1 -- alert 
 // alert(    "external file alert"      )
 // console.log(" dfvgdfvfd")
-
 // 2 -- Variable for string
-
 // alert(names)
 // var names;
 // names = 'khan'
