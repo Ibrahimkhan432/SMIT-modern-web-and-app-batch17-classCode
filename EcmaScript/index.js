a04056edee2376db83e2a4c834987e70a77cba82
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-16 15:54:36 +0500

    Ecma(javascript) intro

---
+++ EcmaScript/index.js
@@ -0,0 +1,14 @@
+// 1 -- alert 
+// alert(    "external file alert"      )
+// console.log(" dfvgdfvfd")
+
+// 2 -- Variable for string
+
+// alert(names)
+// var names;
+// names = 'khan'
+// console.log(names)
+
+// var names = "test"
+// names = "test 2"
+// console.log(names)
