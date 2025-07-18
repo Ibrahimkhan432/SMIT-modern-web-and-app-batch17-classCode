81c545c38a3d931dbf768232df9656d7163eede6
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-18 15:23:37 +0500

    variables with numbers

--- EcmaScript/16-07-2025/index.js
+++ EcmaScript/16-07-2025/index.js
@@ -37,3 +37,4 @@
 // var names = "test"
 // names = "test 2"
 // console.log(names)
+
---
+++ EcmaScript/18-07-2025/index.js
@@ -0,0 +1,134 @@
+
+//  var test =' Hello, World!';
+
+//  var name2 =  'hello name's world';  //errr
+
+// var name2 = "hello names's world  "
+// var name2 = 'hello names"s world  '
+
+
+// var testname = 'hello world'
+
+// console.log(name2)
+// console.log(testname)
+
+
+// name = "mark"
+
+// console.log(name)
+
+// var email = "ibrahim"
+// console.log(final)
+
+// variable name rules
+
+// var name = "ibrahim"
+// var 2name //error
+// var name fname = "ibrahim" //error
+
+// var -name
+// var _name
+// var $name
+
+// var    myNameLast // camel case or dromedary 
+// mynamelastname // flate case
+// MY-NAME-LAST // Train case
+// my_name_last // snake case
+// // pascal case -- MyNameLast
+
+// var mynum = 12
+// console.log(mynum)
+
+// var mynum2 = "12"
+// console.log(mynum2)
+
+// var totalMarks = 1200
+// var percen = 100
+// var contain = 600
+
+// var result = contain /  totalMarks *percen
+
+// console.log(result, "%")
+
+// var num1 = 10
+// var num2 = 20
+
+// console.log(num1 + num2)
+
+
+// var name = "ibrahim"
+// var Name = "khan"  // sensitive case
+// console.log(name,Name)
+
+// alert(3)
+
+// var na3me
+
+
+// var name = "sdivb"
+// var let  //error
+
+// var name = "name1"
+
+// var name = "name2" //redeclare
+
+// name ="name3"  //reassign
+
+// var myname = "ibrahim"
+
+// var myname = "khan"
+
+// console.log(myname)
+
+// let myname = "ibrahim"
+
+// myname = "khan"
+
+// console.log(myname)
+
+// const names = "ibrahim"
+
+// const  names = "khan"
+
+// console.log(names)
+
+// var num = 2
+// var num2 = 3
+
+// var value = num + num2
+// console.log(value)
+// console.log(num + num2)
+// alert(value)
+// alert( "2 " + "3 ")  //concatenate
+
+// console.log(4 / 2)
+
+// console.log(5 % 2)
+
+// console.log(2/0) //INFINITE
+
+// var number = 2
+
+// var result = number - 1
+// --number // pre decrement
+// ++number // pre increment
+// alert(number)
+// number ++
+
+// alert(number)
+// number ++
+
+// alert(number)
+
+// var a = 2
+// var b = 3
+// var result = a++ + b + b + --a
+// var result = a++ + (b + b) * --a + (b + (++b - --a))
+//            3 +  (3 + 3) * 1 +  (3 +(4  - 1 ))
+
+// var num3 = a++ + a++     // assignment
+// 20,19,10   (3 + 6) *  2 +   3 +  4  -2
+// console.log(a++ + (b + b) * --a + (b + (++b - --a )))
+
+// console.log(num3)
+
