42a424ba1d0fe4101daf66ecc2dcd32f2b853c48
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-21 15:23:50 +0500

    prompt,comparision and if

---
+++ EcmaScript/21-07-2025/index.js
@@ -0,0 +1,105 @@
+// var names = "ibrahim "
+// var lname ="khan"
+
+// // console.log(names + lname)
+// alert("hi " + names + " how are you ")
+
+// alert("2 plus 2 is " + 2 + 2)
+// alert(2 - "2" + "2")
+
+// prompt  give message and save response
+// alert  just give a message
+
+// prompt("this is promt","hi")
+
+// var value = prompt("enter number")
+// console.log(value)
+
+// var value = prompt()
+// console.log(value)  // output null
+
+// var value1 = prompt("entern value 1")
+// var value2 = prompt("entern value 2")
+
+// var value1 = Number(prompt("entern value 1"))
+// var value2 = Number(prompt("entern value 2"))
+
+// var value1 = +(prompt("entern value 1"))
+// var value2 = +(prompt("entern value 2"))
+
+// var value1 = prompt("entern value 1")
+// var value2 = prompt("entern value 2")
+
+
+// var value11 = Number(value1)
+// var value22 = Number(value2)
+
+// var result = value11 + value22
+// console.log(result)
+
+// window.prompt("test")
+
+// ----------------------if----------------------------
+
+// var userName = prompt("enter you name")
+
+// var num = 2
+
+// if (num === "2") {
+//     console.log("run success")
+// }
+
+// = asssign
+// == equal 
+// === equal data type (string,number)
+
+
+// var  userAge = prompt("enter your age")
+
+// if(userAge == "10"){
+//     alert("you are not allowed")
+// }
+
+// var primeMinister = prompt("enter your name")
+
+// var primeMinister ="imran khan"
+
+// if(primeMinister !== "shehbaz"){
+//     alert("you are not prime minister")
+// }
+
+// var studentName = prompt("enter your name")
+
+// if (studentName === "hasnain") {
+//     console.log("sorry you are not allowed")
+// }
+
+// else if (studentName === "sadiq") {
+//     console.log("sorry you are not allowed")
+// }
+// else {
+//     console.log("you are allowed")
+// }
+
+
+// var age = prompt("enter your age")
+
+// if(age >= 10){
+//     console.log("you are young man")
+// }
+// if(age < 10){
+//     console.log("you are young boy ")
+// }
+
+// if(age>10)  console.log("you are boy")
+
+// if (age > 10) { console.log("you are boy") }
+
+
+
+
+// Assignment demo  1 -11
+
+// 1.Alert
+
+// alert("hello world")
