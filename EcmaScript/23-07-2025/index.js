e8351208a17cecbcd50dfa0a1e3dd20a243bdb4b
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-23 14:48:24 +0500

    Revision chap 1 to 11

---
+++ EcmaScript/23-07-2025/index.js
@@ -0,0 +1,130 @@
+// Revision chap 1 to 11
+
+
+// var userName = "ibrahim"
+
+// alert(userName)
+
+// var pakistan ="ibrahim"
+
+// alert(pakistan)
+// console.log(pakistan)
+// window.alert("test")
+
+// var number = 10;
+// var number2 = "10";
+// console.log(number)
+// console.log(number2)
+
+// var names =" anas"
+// var age = 10
+
+// console.log("my name is " + names + " and my age is " + age)
+
+
+
+// illegal names
+
+// var 1name 
+// var name lname 
+// var -my
+
+// var var 
+// var const  = "name"
+// console.log(let)
+
+// legal name
+
+// mynameis
+// mylastname33
+// var name23
+// var na3me_
+// var _na3me
+// var $na3me
+
+// myNameIsIbrahim   -- camel case (dromedary)
+// kabab case  my_name
+
+// a definitive guide js book
+// https://pepa.holla.cz/wp-content/uploads/2016/08/JavaScript-The-Definitive-Guide-6th-Edition.pdf
+
+// var Name = "ibrahim"
+// var name = "ibrahim khan"
+
+
+// var number = 3 
+// var number2 = 4
+
+// console.log(number + number2)
+
+// var number = (1 + 2) * 2 + (2 * 2)
+// console.log(number)
+// var number = 3 / 1
+// var number = 2 % 2
+// console.log(number)
+
+// var number = 2
+// var number2 = number + 1
+// console.log(number2)
+
+// number = 3
+// var num = number + 1
+// console.log(num)
+
+// var num = 2
+// num++
+// console.log(num)
+
+
+// a++  post increment
+// a--  post deccrement
+// ++a  pre increment
+
+
+// var num = 2
+
+// console.log(++num) // 3
+// console.log(num) // 3
+
+
+// // var num = 2
+
+// // console.log(--num) // 1
+// // console.log(num) // 1
+
+// // console.log(++num)
+
+// var value1 = 3
+// var value2 = 4
+
+
+// // var result = value1++ + value2++ // 7
+// // console.log(value1++ + value2++) // 9
+// // var result = value1-- + value2-- + value1++
+// // console.log(value1-- + value2--)
+
+// console.log(result) // 
+
+var a = 2
+var b = 3
+
+// var result = ++a + --b + ++b + a++ + ++a + a++ + a++
+//    //   3 + 2 + 3 + 3 + 5 + 5 + 6
+// console.log(result)
+
+// var result = ++a + a++ + ++a
+
+// // console.log(++a + a++ + ++a)
+// console.log(result)
+
+// var uSERnAME = "KHAN"
+// prompt(uSERnAME, "ibrahim")
+
+// var userName =  prompt("enter your name")
+
+// if(userName == "Ibrahim"){
+//    console.log("chal gaya") 
+// }
+// else {
+//     console.log("nhi  chala")
+// }
