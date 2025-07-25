851528b129e5cc2a3771bbaabacb5401b25ea968
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-25 15:53:56 +0500

    if else , nested condition

---
+++ EcmaScript/25-07-2025/index.js
@@ -0,0 +1,139 @@
+// var x = 20
+// // var y = x + 10
+// // x += 10   // compound assignment or compound operator
+// console.log(x)
+
+// if("ab"<"ac"){
+// console.log("codition true")
+// }
+// else {
+//     console.log("codition false")
+// }
+
+
+
+// var num = 10 ;
+
+// if ( num >= 10){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// var num = (prompt("enter number"));
+
+// if ( num != 10){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+// var num = (prompt("enter number"));
+// if (num == 10) {
+//     console.log("10")
+// }
+
+// else if (num == 20){
+// console.log("20")
+// }
+
+// else if (num == 30){
+// console.log("30")
+// }
+
+// else if (num == 40){
+// console.log("40")
+// }
+
+// else {
+//     console.log("false")
+// }
+
+
+// calculator
+
+// var num1 = Number(prompt("enter first number"))
+
+// var operator = prompt("enter operator + , - , / , %")
+
+// var num2 = Number(prompt("enter second number"))
+
+// if (operator === "+") {
+//     console.log(num1 + num2)
+// }
+// else if (operator === "-") {
+//     console.log(num1 - num2)
+// }
+
+// else{
+//     console.log("enter a valid operator")
+// }
+
+
+// && // both should be true
+// || // either first or last  be true     // gates
+
+// var studentName = "anas"
+// var rollNumber = 10
+// var subject = "urdu"
+
+
+// if( (studentName =="anas" && rollNumber == 10) || subject == "math"){
+//     console.log("valid student")
+// }
+// else{
+//     console.log("not valid student")
+// }
+
+
+// var marks = Number(prompt("enter your marks"))
+// var subject = (prompt("enter your subject"))
+
+// if ((marks >= 89 && marks < 100 ) || (subject =="math" || subject == "urdu"  ) ) {
+// if(marks > 88 && marks <= 100 ){
+//     console.log("Grade A1")
+// }
+// else if (marks > 80 && marks < 89) {
+//     console.log("Grade A")
+// }
+// else if (marks > 70 && marks < 80) {
+//     console.log("Grade B")
+// }
+// else if (marks > 60 && marks <= 70) {
+//     console.log("Grade C")
+// }
+// else if (marks < 60) {
+//     console.log("Failed")
+// }
+// else {
+//     console.log("enter a valid marks")
+// }
+
+// assignment
+// write a condition if user try to give more than 100 
+// the console should show the message (please enter a number between 1 to 100)
+
+
+// var age = Number(prompt("enter your age"))
+
+// var city = prompt("enter your city")
+
+// if ((age > 10 || age < 20) || city == "karachi") {
+
+//     console.log("yes you are from karachi")
+// }
+// else {
+
+//     console.log("you are not from karachi")
+// }
+
+var num1 = Number(prompt("enter num1"))
+var num2 = Number(prompt("enter num2"))
+
+if (num1 > num2) {
+    console.log(num1 + " is greater than " + num2)
+}
+else{
+    console.log("dimagh sahi he ?")
+}
