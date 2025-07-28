9e82c8770a4dace02816e988b7c2351fa5e92210
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-28 14:49:39 +0500

    Array Intro with data types concept

---
+++ EcmaScript/28-07-2025/index.js
@@ -0,0 +1,101 @@
+// var student1 = "ali"
+// var student2 = "anas"
+
+// var students = [ "ali","anas"]  // length = 2 , index = 1
+
+// // console.log(students)
+//  console.log(students.length)
+//  console.log(students[2])  // index starts from 0
+
+// students[0] = "bilal"
+// console.log(students)
+
+// var students = ["ali",3, true,]    //untyped 
+// console.log(arr2)
+
+
+// primitive data testing
+// var student = "ibrahim"
+
+// var student2 = student
+// student2 = "khan"
+// console.log(student2)
+// console.log(student)
+
+// var arr1 =["ibrahim"]
+
+// var arr2 = arr1
+// arr2[0] = "khan"
+// console.log(arr1)
+// console.log(arr2)   
+// non-primitive data
+//copy by refrence
+// shallow copy
+// mutable
+
+// var arr = [{name :"ali"},{age : "20"}]
+
+// var arr = ["A", "B", [1,2,3 ]]  // nested array
+
+// console.log(arr.length)
+// console.log(  arr[2][1]  )  // 2*2 matrix
+
+// var matrix0 = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ]
+// var matrix1 = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ]
+
+// console.log(matrix0[1][2]  + matrix1[0][2])
+//  add, subtract , multiply  // assignment
+
+// var name = "ibrahim khan"
+// console.log(name[8])
+
+// var arr =["a","b","c"]
+// arr[1]="p" //mutable
+// var arr2 = [...arr]  // spread operator 
+
+// var arr2 = ["a","b","c"]
+
+// console.log(arr2)
+
+
+// var base = 1024;
+// var table = [base, base+1, base+2, base+3];
+// console.log(table)
+
+// constructor array
+// var a =new Array()
+// a[0] = "test"
+// a[1] = "test 2"
+// a[3] = "test 3"
+// console.log(a[2])
+// console.log(a)
+
+
+// adding
+// var arr = ["a", "b", 3, 4, { name: "ibrahim" },[1,2,3]]
+
+// arr.unshift("start")  //add in first
+// arr.push("test")  //add in last
+
+// remove
+// arr.shift() //remove in first 
+// arr.pop() //remove in last
+
+// unshift shift ka dost 
+//push pop ka dost he 
+
+
+// console.log(arr)
+
+
+
+
+
