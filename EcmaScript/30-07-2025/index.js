438498dfde85bf63b8e3ffcc14b58185ac0ef135
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-07-30 14:46:29 +0500

    array , splice , slice

---
+++ EcmaScript/30-07-2025/index.js
@@ -0,0 +1,82 @@
+// array
+
+// var student = "ahmed"
+// var student2 = "ali"
+
+// array
+
+// var students = [ "ahmed","ali","anas","bilal","jawed"]//total indexes 4
+//total length 5
+
+// console.log(students)
+// console.log(students.length)
+// console.log(students[0])
+
+// add
+// students[5] = "shafiq"
+// console.log(students)
+
+// var arr=[]
+
+// arr[0] = "a"
+// arr[1] = "b"
+// console.log(arr)
+
+// var name = "ibrahim"
+// var names = name  
+// names = "change"
+
+// console.log(name)
+// console.log(names)
+
+
+// var arr = ["a", "b"]
+// // var newArr = arr;
+// var newArr = arr.push("c, d")
+// // newArr=["c","d"] //mutable
+// //  // copy by refrence
+// //  //shallow copy 
+// console.log(newArr)
+// console.log(arr)
+
+// var arr = [1,2,3,4,5]
+
+// arr.push(6) //add in last 
+// arr.unshift(0) //add in last 
+
+// arr.pop()
+// arr.shift()
+// console.log(arr)
+
+// splice
+// var arr = [1,2,3,4,5]
+// console.log(arr.splice(1,2, "a","b"))
+// console.log(arr)
+// // 0 means index , 1 means how many elements will remove(quantity)
+
+// var arr = ["a", "b", "c", "d","e","f"]
+// console.log(arr.slice(0,2))  // slice make new array
+// console.log(arr.slice(0,5))  // slice make new array
+// 0 and 2 are indexes 
+
+// slice  (1,3) 1,2    
+// (1,3) = (3-1)
+
+// var arr = ["a","b"]
+// console.log(arr.indexOf("a")) // -1 means no value included
+// console.log(arr.includes("c")) //  includes return bool
+
+
+// var myName ="ibrahim"
+// console.log(myName.indexOf("b"))
+// console.log(myName.includes("u"))
+
+
+//ES6 
+// var arr = ["a","b",     "c","d"]
+// var newArr = ["khan",...arr]   //spread operator
+// console.log(newArr)
+
+// var arr = [1, 2, 3, 4, 5, ["a", "b", "c", "d"]]
+
+// console.log(arr[5][1])
