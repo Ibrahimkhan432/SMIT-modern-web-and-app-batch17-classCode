47dc85fb11b78a5bbb610a4805b13bdb68bdad4b
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-04 15:16:40 +0500

    flags.boolean,string in forLoop

---
+++ EcmaScript/04-08-2025/index.js
@@ -0,0 +1,64 @@
+
+//  write a variable of your name
+// print one by one characters of your name 
+
+// for (var i = 0; i< 5; i++) {
+//     console.log(i)
+// }
+// console.log(i)
+
+// var myName = "ibrahim"
+// var myName = prompt("enter your name")
+// var myName = [1, 2, 3, 4, 5]
+// for (var i = 4; i >= 0; i--) {
+
+//     console.log(myName[i])
+// }
+// console.log(myName[i])
+
+// var numbers = [1, 2, 3, 4, 5]
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//     // console.log(numbers[i] * numbers[i])
+// var square = numbers[3] * numbers[3]
+//  //            4 * 4
+//     // square = 1 
+// var sum = sum + square
+// //          1   +    9
+// console.log(square)
+// }
+// console.log(sum)
+
+// var students = ["ali", "anas", "asad", "abid", "bilal", "saad", "saim"]
+
+// var target = prompt("enter your name")
+// // var target = "kashif"
+// // var target = "bilal"
+// var studentFound = false
+// for (i = 0; i < students.length; i++) {
+
+//     if (students[i] == target) {
+
+//         console.log(target,"please mark your attendance")
+//         studentFound = true
+//         break
+//     }
+//     console.log(students[i])
+// }
+// if(studentFound == false){
+//     console.log("student not found")
+// }
+
+
+// --------nested for loop ------
+// var firstName = ["ibrahim ", "bilal"]
+// var lastName = ["khan ", "ashrafi"]
+// var fullName = [];
+// for (var i = 0; i < firstName.length; i++) {
+//     for (var j = 0; j < lastName.length; j++) {
+//         // var fullName = firstName[i] + lastName[j]
+//         fullName.push(firstName[i] + lastName[j])
+//     }
+// }
+// console.log(fullName)
+
---
+++ EcmaScript/1-08-2025/inddex.js
@@ -0,0 +1,51 @@
+//  for loop 
+//loop means
+
+// var students = ["ali", "asad", "bilal", "rehan", "anas", "ali"]
+// console.log(students[0])
+
+//loop -- fundamental of programming
+
+// if( ){
+// }
+
+
+// for (var a = -1; a > -5; a--) {        // (icu)--(initialize ;condition;updation)
+//     console.log(a)
+// }
+
+// var numbers = [10,20,30,40]
+// var target = 30
+// var students = ["ali", "zafar", "saad", "zain"]
+
+// for (i = 0; i < students.length; i++) {
+//     console.log(students[i])
+// }
+
+
+// var numbers = [1, 2, 3, 4, 5]
+// var sum = 0
+// for (i = 0; i < numbers.length; i++){
+// sum = sum + numbers[0]
+// console.log(numbers)
+// }
+// var students = prompt("enter your name")
+
+// for(i=0; i<students.length; i++ ){
+// console.log(students)
+// }
+
+var numbers = [1, 2, 3, 4, 5]
+// total = 0
+// for (i = 0; i < numbers.length; i++) {
+//     total = total + numbers[i]
+//     console.log(total)
+// }
+// console.log(total)
+for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 3) {
+        console.log("greater than 3😍", numbers[i])
+    }
+    
+}
+
