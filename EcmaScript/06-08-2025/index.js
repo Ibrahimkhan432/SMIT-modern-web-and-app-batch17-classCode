bc753484e5fd9386e2f9c7391a4cf8324c7bfee0
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-06 14:51:04 +0500

    string changing case for loop

---
+++ EcmaScript/06-08-2025/index.js
@@ -0,0 +1,65 @@
+// --------nested for loop ------
+// var firstName = ["ibrahim ", "bilal "]
+// var lastName = ["khan ", "ashrafi"]
+// var fullName = [];
+// for (var i = 0; i < firstName.length; i++) {
+//     for (var j = 0; j < lastName.length; j++) {
+
+//         if(i === j){
+//             fullName.push(firstName[i] + lastName[j])
+//         }
+//     }
+// }
+// console.log(fullName)
+
+//  table of 2
+// for (var i = 2; i <= 2; i++) {
+
+//     for (var j = 1; j <= 5; j++) {
+
+//         console.log(i + " X " + j + " = " + i * j)
+// } ///
+// console.log(j)
+// }
+// task  -- print (inverse table)
+
+// var arr = [1,2,3]
+// console.log(arr[0])
+
+// var thoDimensionArray = [
+//     [1, 2],
+//     [3, 4],
+//     [5, 6]
+// ]
+
+// // console.log(thoDimensionArray[0][1])
+// for (var i = 0; i < thoDimensionArray.length; i++) {
+//     // console.log(thoDimensionArray[i][i])
+//     for (var j = 0; j < thoDimensionArray[i].length; j++) {
+//         console.log(thoDimensionArray[i][j])
+//     }
+// }
+// // task -- inverse
+// var isAvailable = false
+// var yourName = prompt("enter your name")
+// var yourName = yourName.slice(0,1).toUpperCase() + yourName.slice(1).toLowerCase()
+// var students = ["Ali", "asad", "saim", "bilal"]
+
+// for (let i = 0; i < students.length; i++) {
+//     if (students[i] === yourName) {
+//         console.log(yourName)
+//         isAvailable = true
+//     }
+// }
+// if (isAvailable == false) {
+//     console.log("not available")
+// }
+
+// for (i = 0; i <= 5; i++) {
+//     stars = ""
+//     for (j = 1; j <= i; j++) {
+//         stars = stars + "* "
+//     }
+//     console.log(stars)
+// }
+
