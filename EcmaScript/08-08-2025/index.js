41ab08b6bbb5e0fadfa20585333d95ee68227d4a
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-11 12:21:58 +0500

    string (slice, indexOf, replace)

---
+++ EcmaScript/08-08-2025/index.js
@@ -0,0 +1,45 @@
+
+var warName = "World War II"
+// var newWarName = warName.slice(0,12)
+// for (var i = 0; i < warName.length; i++) {
+
+//     if (warName.slice(i, i + 12) == "World War II") {
+//         warName = warName.slice(0, i) + "the Second World War" + warName.slice(i + 12);
+
+//     }
+
+// }
+// console.log(warName)
+
+
+//  --- indexOf---
+// var newWarName = warName.indexOf("World War II")
+// console.log(newWarName)
+// if (newWarName !== -1) {
+//     warName = warName.slice(0, newWarName) + "the Second World War" + warName.slice(newWarName + 12);
+//     console.log(warName)
+// }
+
+// var myName = "i$brahim"
+// // var myNameFirstChar = myName.charAt(myName.length - 1)
+// for (var i = 0; i < myName.length; i++) {
+//     if (myName.charAt(i) === "$") {
+//         console.log("$ is not allowed")
+//         break
+//     }
+//     console.log(myName)
+// }
+
+// var warName = "World War II"
+
+// var newWarName = warName.replace("World War II", "The Second World War")
+
+// console.log(newWarName)
+
+
+var paragraph = "my name is ibrahim and my friend name is also ibrahim and my another friend name is also ibrahim"
+
+var changeMyName = paragraph.replaceAll("ibrahim", "bilal")
+
+console.log(changeMyName)
+//assignment -- change using for loop 
