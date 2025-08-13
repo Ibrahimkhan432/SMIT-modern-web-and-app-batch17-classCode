b98c1624a13358d782445aa05aae4ed66247c8a5
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-13 17:06:34 +0500

    Numbers - intro IEEE concept , rounding numbers

---
+++ EcmaScript/13-08-2025/index.js
@@ -0,0 +1,32 @@
+
+// var array = [1, 2, 3]
+
+// var newArr = array.map((number) => {
+//     console.log(number)
+// })
+
+//  map,filter,reduce,indexOf, -(hof --high order function)
+// we will cover these funtions in function topic 
+
+
+// "234567890"
+
+// var number = Math.random()
+// console.log(number)
+
+// 16 decimal standard 
+
+// IEEE 574  - institute of electrical and electronic engineering
+//Math.random cover 64 bit space 
+
+// var number = Math.random() * 10
+// var number = Math.round(23.45) 
+// var number = Math.round(.653456789) 
+// var number = Math.ceil(0.9) 
+// var number = Math.floor(0.9) 
+// console.log(number)
+
+// var num = Math.ceil(Math.random() * 6)
+// var num = Math.floor(Math.random() * 6)
+// var num = Math.round(Math.random() * 6) + 1
+// console.log(num)
