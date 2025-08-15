1c2d00339956765292d69e9c9949e04a9c851f1a
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-15 14:59:59 +0500

    Numbers - converting methods

---
+++ EcmaScript/15-08-2025/index.js
@@ -0,0 +1,33 @@
+
+// var number =Math.floor( Math.random() * 6 + 1)
+// console.log(number)
+
+// console.log("30" + 30)
+// var age = prompt("enter your age")
+// var age = 45.34
+// var ageInNumber = parseFloat(age)
+// console.log(ageInNumber + 5)
+
+// var number = 1.5555
+// console.log(number.toFixed(1))
+
+// var number = 20000.3456789
+
+// // var numberInString = number.toString()
+// console.log(number.toFixed(2))
+// console.log(numberInString + "2")
+
+// console.log(number.length)
+
+// properties -- for information 
+// method -- for process
+
+// var num = 1.555
+
+// var str = num.toString();
+// if (str.charAt(str.length - 1) === "5") {
+//     str = str.slice(0, str.length - 1) + "6";
+// }
+// num = Number(str);
+// prettyNum = num.toFixed(2);
+// console.log(prettyNum)
