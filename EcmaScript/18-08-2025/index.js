f250aa08936f0601ca99445b1c4685d298c4e855
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-18 15:25:48 +0500

    Date and Time , intro , get methods

---
+++ EcmaScript/18-08-2025/index.js
@@ -0,0 +1,32 @@
+// var today =new Date()
+// var todayInString = today.toString();
+// var todayDate = todayInString.slice(0,8)
+// var todayDay = todayInString.slice(0,15)
+// console.log(todayDay)
+
+
+// epoch unix time  (1 january 1970)
+
+
+// var days = ["sunday", "monday", "tuesday"]
+
+var rightNow = new Date()
+//  var todayDay = rightNow.getDay()
+//  var nameOfTodayDay = days[todayDay]
+//  console.log(nameOfTodayDay)
+// console.log(days[rightNow.getDay()]) // shorthand
+
+// console.log(rightNow.getMonth())
+// assignment to get month in valueform(january)
+// console.log(rightNow.getFullYear())
+// console.log(rightNow.getMinutes())
+// console.log(rightNow.getSeconds())
+// console.log(rightNow.getMilliseconds())
+// console.log(rightNow.getHours())
+
+// var customeDate = new Date("20 jan  2020")
+
+// var currentDate = new Date()
+
+// console.log(currentDate - customeDate)
+
