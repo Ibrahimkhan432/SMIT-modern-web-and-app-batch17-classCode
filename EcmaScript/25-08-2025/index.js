728b95486e43be9a5ebca730286674115fffb106
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-25 14:43:24 +0500

    functions , passing data , scoping

---
+++ EcmaScript/25-08-2025/index.js
@@ -0,0 +1,79 @@
+
+// var name = "ibrahim"
+// var age = 20
+// function test(name,age) {
+//     console.log(`my name is ${name} and age is ${age}`)
+// }
+// test( name,age)
+
+// function addNumber(a, b) {
+//     //    console.log( a + b)
+//     // var result = a + b
+//     console.log("sjdfgbhs")
+//     return a + b
+// }
+
+// console.log(addNumber(2, 3))
+
+// var stdName = "ali"
+// function checkName(stdName) {
+
+// if(stdName==="ali"){
+//     console.log(stdName)
+//     return stdName
+// }
+// }
+// console.log(checkName(stdName))
+
+// var firstName = "Ibrahim"
+// var lastName = prompt("enter your last name ")
+
+// function myName(firstName,lastName){
+
+//     var fullName = firstName + lastName
+//     // console.log(fullName)
+// }
+// console.log(fullName) // not define error
+// myName(firstName,lastName)
+
+
+// function sum(fvalue,lvalue) {    //parameter
+
+// console.log(fvalue , lvalue)
+
+// }
+
+// var fname = "ibrahim"
+// var lname = "khan"
+
+// sum(lname,fname) //argument  consider as a order not a value
+
+
+// if (true) {
+//     // console.log(name)
+// }
+
+// function test() {   // pure function
+//     // return name + Date(),to String,random    -- >impure function
+//         let name = "ali"
+// }
+// console.log(name)
+// test()
+// sum = undefined
+// var sum;
+// test()
+// function test() {
+//      sum = 2 + 4
+// }
+// console.log(sum)
+
+
+// ----arrow function--
+
+var arr = [1, 2, 3, 4, 5]
+
+arr.map((value) => {
+
+    var mult = value * 5
+    console.log(mult)
+})
