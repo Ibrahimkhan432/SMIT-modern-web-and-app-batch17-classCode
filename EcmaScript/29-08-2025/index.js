e9ea1e7fadf94e9f0086111c9fbf0617de8156a7
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-27 16:08:19 +0500

    Switch Case , Loops , Event

---
+++ EcmaScript/29-08-2025/index.js
@@ -0,0 +1,51 @@
+// var day = "tues";
+
+// var day = prompt("enter a day ")
+
+// switch (day) {
+//     case "mon":
+//     case "tues":
+//         console.log("today is monday or tuesday");
+//         break
+// case "tues":
+//     console.log("today is tuesday");
+// break
+//     case "wed":
+//         console.log("today is wednesday");
+//     default:
+//         console.log("enter a write day");
+// }
+// if else
+// if condition complex
+// slow for single condition
+// more flexible for complex
+
+// switch 
+// if single condition
+// slow for complex condition
+// more flexible for single condition
+/*
+main difference between switch and if-else is  (default)
+// else execute when if false
+default execute when we need to show false condition 
+*/
+
+// -----while loop ---- 
+
+// var i = 2
+// // while (i < 2) {
+// //     console.log(i)
+// //     i++
+// // }
+
+
+//  -- do while ---
+
+// do {
+//     console.log(i) //2 
+//     i++ 
+//} while (i< 0)  //jdhyfygewygdwe
+
+// function func(){
+//     document.getElementById("para").innerHTML = "testing"
+// }
