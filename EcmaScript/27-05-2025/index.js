e7ad04168a33140eeb5ebc8e0f17e09cc4f13742
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-08-27 16:06:26 +0500

    ES6 advance function , methods , assignemt chap 21 to 30

---
+++ EcmaScript/27-05-2025/index.js
@@ -0,0 +1,84 @@
+// function add() {
+// console.log("test")
+// }
+// add()
+
+// function add(value) {
+// console.log(value)
+// }
+// add("test")
+
+// var value = 3
+// var num = 2
+
+// function add(value,num) {
+
+//     var result = value + num
+//     console.log(result)
+// }
+// add(value,num)
+
+//  anonymous function    //es5 function
+// let func = function (num1, num2) {
+//    return num1 + num2
+// }
+// console.log(func(2, 3))
+
+// es6 
+
+// let funct = (a, b) => {
+//   return  a + b
+// }
+// console.log(funct(2, 3))
+
+// let funct = (a, b) =>  a + b 
+// console.log(funct(2, 3))
+
+// let y = (x) => x * x
+// console.log(y(3))
+
+// high order functions
+// var arr = [1, 2, 3, 4]
+// var arrMap = arr.map((x) => {
+//     return x = x * 2
+// })
+// console.log(arrMap)
+
+// var arrFilter = arr.filter((x)=> x > 2)
+// console.log(arrFilter)
+
+// var arrReduce = arr.reduce((acc,curr)=> acc + curr)
+// console.log(arrReduce)
+
+// remaining methods will be consider in assignment
+
+
+// hoisting
+
+// var func = (num) => {
+//     console.log(num)
+// }
+// func(2)
+
+
+// function add(num){
+//     console.log(num)
+// }
+// add(2)
+
+// {
+// var a = 2
+// console.log(a)
+// }
+
+
+// function func1(func2) {
+//     console.log("func 1")
+//     func2()
+// }
+
+// function func2() {
+//     console.log("func 2")
+// }
+// func2(func1)
+
