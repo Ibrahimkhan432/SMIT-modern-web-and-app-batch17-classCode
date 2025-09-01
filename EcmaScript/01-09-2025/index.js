cc15472b9a90658b8589329a8a85afe5847687da
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-01 14:45:48 +0500

    Events , tag handler in deep

---
+++ EcmaScript/01-09-2025/index.js
@@ -0,0 +1,9 @@
+function showImg(value) {
+    console.log(value.target.src )
+    var result = document.getElementById("pictureId").src = "https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fd1e00ek4ebabms.cloudfront.net%2Fproduction%2F063550c8-61f7-4d50-b0e7-55493c063544.jpg?source=next-article&fit=scale-down&quality=highest&width=700&dpr=1"
+    console.log(result)
+}
+
+function anotherImg() {
+
+}
