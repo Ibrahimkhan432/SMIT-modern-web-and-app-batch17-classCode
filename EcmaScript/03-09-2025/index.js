e4a71440e8afe62791b33402dcc9d6790320c2ae
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-03 16:13:48 +0500

    field events

---
+++ EcmaScript/03-09-2025/index.js
@@ -0,0 +1,86 @@
+// function showImg(){
+//     console.log(event.type)
+// if (event.type==="mouseover") {
+//     event.target.src = "https://quickinsure.s3.ap-south-1.amazonaws.com/uploads/static_page/a83d207a-a933-41ac-a446-db9d23682693/Ktm%20Upcoming%20Bikes%20In%20India%202023%20New%20Launches%20And%20Bike%20Insurance.png"
+// } else {
+//       event.target.src = "https://deinfa.com/wp-content/uploads/2024/06/A-Guide-to-Electric-Cars-in-Pakistan-Featured-Image.png"
+// }
+// }
+
+// var isActive = true //flag
+// function showImg(){
+// if(isActive==true){
+//   event.target.src = "https://quickinsure.s3.ap-south-1.amazonaws.com/uploads/static_page/a83d207a-a933-41ac-a446-db9d23682693/Ktm%20Upcoming%20Bikes%20In%20India%202023%20New%20Launches%20And%20Bike%20Insurance.png"
+
+// isActive = false
+// }
+// else{
+//  event.target.src = "https://deinfa.com/wp-content/uploads/2024/06/A-Guide-to-Electric-Cars-in-Pakistan-Featured-Image.png"
+
+// isActive = true 
+// }
+
+// }
+
+
+// function colorChange(){
+//   var res = document.getElementById("link").style.backgroundColor = "red"
+//   console.log(res)
+// }
+
+
+// function detailedPara() {
+
+//    var paragraph = document.getElementById("para").innerText = "pakistan is a largest ghareeb idafgguifuidfgiudgfidugfidsfgsduifhbduksjbvdbvjd"
+
+//    var sliceValue = paragraph.slice(0,20)
+//    console.log(paragraph)
+//     console.log(sliceValue)
+// }
+
+// function textFocus(){
+//     document.getElementById("textField").style.background = "red"
+// }
+
+// function result() {
+//     var username = document.getElementById("name").value
+//     var userEmail = document.getElementById("em").value
+
+//     if (username === "" || userEmail === "") {
+//         alert("are name to likho")
+//     }
+//     console.log(username)
+//     console.log(userEmail)
+// }
+
+// function checkCampus() {
+
+//     var stdName = document.getElementById("name").value
+//     // var campus = document.getElementById("campus").value
+//     var campus;
+//     switch (stdName) {
+//         case "10":
+//             campus = "zaitoon"
+//             break;
+//         case "20":
+//             campus = "gulshan"
+//     }
+//     document.getElementById("campus").value = campus
+// }
+
+// function checkCampus() {
+
+//     var stdName = document.getElementById("name").value
+//     var campus = document.getElementById("campus").value
+//     switch (stdName) {
+//         case "10":
+//             campus = "zaitoon"
+//             break;
+//         case "20":
+//             campus = "gulshan"
+//     }
+// }
+
+// function insertImage(){
+//     document.getElementById("img").innerHTML = `<img src="https://www.shop4tesla.com/cdn/shop/articles/rennen-um-die-globale-e-auto-vorherrschaft-wird-byd-2024-tesla-schlagen-260668.jpg?v=1720619629" width=400px alt="">`
+// }
