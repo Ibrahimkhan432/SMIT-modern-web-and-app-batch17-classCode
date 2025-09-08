dd433c9891b62e4325c5bd975189d71b533f5b2e
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-08 15:04:11 +0500

    Revision chap 31-40

---
+++ EcmaScript/08-09-2025/index.js
@@ -0,0 +1,142 @@
+
+// // Individual Check Conditions
+// // function result() {
+// //     var username = document.getElementById("name").value
+// //     var userEmail = document.getElementById("em").value
+
+// //     // if (username === "" || userEmail === "") {
+// //     //     alert("incomplete credentials")
+// //     // }
+// //     if (username == ""){
+// //         alert("Please Enter Your Name")
+// //     }
+// //     // else if (userEmail == ""){
+// //     //     alert("Please Enter Your Email")
+// //     // }
+// //     // else {
+// //     //     alert("Form Submitted Sucessfully")
+// //     // }
+
+// //     console.log(username)
+// //     // console.log(userEmail)
+// // }
+
+// // Individual Check Name and Email
+// function result() {
+//     var username = document.getElementById("name").value
+//     if (username == ""){
+//         alert("Please Enter Your Name")
+//     }    
+//     console.log(username)
+// }
+// function checkEmail(){
+//     let email = document.getElementById("en").value
+//     if (email == ""){
+//         alert("Please Enter Your Email")
+//     }
+//     else {
+//         alert("Email is Submitted")
+//     }
+//     console.log(email)
+// }
+
+// var getTime = new Date()
+// console.log(getTime)
+// function showTime() {
+//     var getTime = new Date()
+//     console.log(getTime)
+//     document.getElementById("time").innerText = getTime
+// }
+
+// function userName(vale,vale2) {
+//     console.log(vale,vale2)
+// }
+
+// userName(name,age)
+
+// let age = 30
+// let name = "test"
+
+
+
+// function test() {
+//     var name = "a"
+//     var age = 3
+//     var subj = "math"
+//     console.log(name, age, subj)
+// }
+// test()
+
+// let funct = (a, b) => {
+//     console.log(a + b)
+//     return a + b
+
+// }
+// funct(2, 3)
+// console.log(funct(2, 3))
+
+// ----hoisting ---
+// var a = 3
+// a = 40
+
+// let a = 4
+// let a = 5
+
+// console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
