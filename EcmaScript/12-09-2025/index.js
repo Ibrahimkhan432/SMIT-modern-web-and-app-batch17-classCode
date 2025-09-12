eb28e3d73691f8449aa82c992f02d5d441d17d64
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-12 15:49:34 +0500

    manipulate events , images css

---
+++ EcmaScript/12-09-2025/index.js
@@ -0,0 +1,9 @@
+// function hidePic(event) {
+//     var cat = document.getElementById("cat").className += " hide"
+//     console.log("cat--", cat)
+//     console.log("event--", event)
+// }
+function swapPic(id,picture) {
+    var changePic = document.getElementById(id).src = picture
+
+}
