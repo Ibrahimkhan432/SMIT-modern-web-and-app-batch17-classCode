ed6a5b097546611f885a84d1276f644427590bcf
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-17 15:46:17 +0500

    Dom Introduction ,finding  parent childs

---
+++ EcmaScript/17-09-2025/index.js
@@ -0,0 +1,33 @@
+//  DOM
+//document(index file)
+//  interaction with HTML
+// construct , modify ,flexibility ( through JS)
+// form validation , content (dynamic),heirarchy ,tree structure
+
+// BOM 
+// browser object model
+// console
+// alert
+
+// removed junks--> (mignification)
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[5].childNodes[1].nodeName);
+
+// parentNode
+// console.log(document.childNodes[1].parentNode)
+
+// var div = document.getElementById('divs')
+// var data = div.childNodes[1].childNodes[1].innerHTML
+// console.log(data)
+
+// addition
+// function add() {
+//     var value1 = Number(document.getElementById('val1').value)
+//     var value2 = parseInt(document.getElementById('val2').value)
+//     var sum = value1 + value2
+//     document.getElementById('result').innerHTML = "The sum is " + sum
+//     console.log(sum)
+// }
+
+// var data = document.getElementsByTagName('p')
+// console.log(data[1].innerText)
