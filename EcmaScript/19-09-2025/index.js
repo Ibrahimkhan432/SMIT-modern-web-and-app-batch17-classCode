ee338dc4a0f32c5a84283049f5ea61a9b9dcbe7c
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-19 16:06:43 +0500

    Child elements modification , create elements

---
+++ EcmaScript/19-09-2025/index.js
@@ -0,0 +1,26 @@
+// var d = document.getElementById("div").childNodes[5]
+// var d = document.getElementById("div").nextSibling
+// console.log(d)
+
+
+
+// var d = document.getElementById("div")
+// console.log(d.nodeValue)
+// console.log(d.innerHTML)
+// console.log(d.innerText)
+// console.log(d.textContent)
+
+
+// var d = document.createElement("DIv")
+// var d = document.getElementById("div")
+
+// var newP = document.createElement("p")
+
+// var data = document.createTextNode("This is Js tag")
+
+// newP.appendChild(data)
+// d.appendChild(newP)
+
+var removedChild = document.getElementById("div").childNodes[1]
+console.log(removedChild)
+// var removed = document.removeChild(   )
