ac7e4b62073fa72a9e140aa679d906566586d3a4
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-24 14:47:55 +0500

    Revision Chap 41 - 47

---
+++ EcmaScript/24-09-2025/index.js
@@ -0,0 +1,17 @@
+// i = 8   
+// while (i < 5) {
+//     console.log("chal gaya");
+//     i++
+// }
+
+
+// i = 0
+// do {
+//     console.log("chal gaya", i)
+//     i++
+// }
+// while (i < 5)
+
+function fun(){
+    document.getElementById("para").style.fontSize = "40px"
+}
