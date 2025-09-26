29767e6ec2080aed54fa1f6c4a6e0c09c2921ac1
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-26 15:04:59 +0500

    revision chp 48 - 52

---
+++ EcmaScript/26-09-2025/index.js
@@ -0,0 +1,43 @@
+// function changeColour() {
+//     console.log("user field run")
+//     document.getElementById("userName").style.backgroundColor = "red"
+// }
+
+// function changeText() {
+//     console.log("user field run")
+//     document.getElementById("userEmail").style.fontSize = "20px"
+// }
+
+// check email
+// function checkEmail(emailId){
+// var email = document.getElementById(emailId)
+
+// if(email.value ===""){
+//     alert("email required")
+// }
+// }
+
+
+// check campus
+
+// function checkCampus() {
+
+//     var rollNumber = document.getElementById("rollNumber").value
+//     var campus;
+//     switch (rollNumber) {
+//         case "100":
+//             campus = "zait"
+//             break;
+//         case "101":
+//             campus = "bahadurabad"
+//             break;
+//         default:
+//             break;
+//     }
+//     document.getElementById("campus").value = campus
+
+// }
+
+function visibled(){
+    document.getElementById
+}
