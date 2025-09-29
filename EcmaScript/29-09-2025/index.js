d95f9c83b2b815bd62151852e3f4cb294590b330
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-09-29 15:04:39 +0500

    Revision chp 53 - 61

---
+++ EcmaScript/29-09-2025/index.js
@@ -0,0 +1,51 @@
+
+// function swapImage(imageId,imageSrc){
+
+//   var swapImages =  document.getElementById(imageId)
+// //   console.log(swapImage , event)
+
+// //   swapImages.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGOBDbHCKOYMdN7n9uvVzRcD20sx2_4OM-VQ&s'
+
+// // or
+//   swapImages.src = imageSrc
+
+// }
+
+// function linkFunc() {
+
+//   console.log(document.getElementById("linked").href = "https://facebook.com")
+// }
+
+// container
+
+// function cont() {
+//   var container = document.getElementById("con")
+//   container.className += " container"
+// }
+
+var para = document.getElementsByTagName("p")
+// console.log(para[0].innerHTML = "<ol><li>test</li></ol>")
+// console.log(para[1].innerHTML = "<img src='https://carwow-uk-wp-3.imgix.net/2025-BYD-Sealion-vs-Tesla-Model-Y.jpg' width='300px'>")
+// console.log(para[0])
+// console.log(para[1])
+// console.log(para[2])
+// for (let index = 0; index < para.length; index++) {
+//   para[index].style.backgroundColor = "red"
+//   if(para[index]=== 2){
+//    var para1 =
+//   }
+// }
+
+
+// dom
+
+  
+ var global =  document.childNodes[1].childNodes[4].childNodes[2].previousSibling
+ console.log(global)
+// if(global.nodeType ===3){
+//   // alert("chunks")
+//   global.innerText = "dfghjkl"
+// }
+// else{
+//   alert("tag")
+// }
